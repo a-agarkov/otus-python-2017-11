0c1221b96a2a9e3ef8146eb9a0185b2b4e27119e
__alexey__ <agarkov.alexey@gmail.com>
2017-11-29 21:19:14 +0300

    pep8 compliance checked. added description in English to Poker.

--- AdvancedBasicsPart_I/otus_hw1_poker/tests/poker.py
+++ AdvancedBasicsPart_I/otus_hw1_poker/tests/poker.py
@@ -25,37 +25,6 @@
 # Одна функция уже реализована, сигнатуры и описания других даны.
 # Вам наверняка пригодится itertools.
 # Можно свободно определять свои функции и т.п.
-
-# ### Description
-# The task was to develop functions, which take poker hand of 7 cards and return best hand of 5 cards.
-
-# ### Cards
-# Each card has suit and rank.
-# * Suits: clubs `C`, spades `S`, червы `H`, diamonds `D`;
-# * Ranks: 2, 3, 4, 5, 6, 7, 8, 9, 10 ten `T`, jack `J`, queen `Q`, king `K`, ace `A`;
-#
-# Example:
-# * `AS` - ace of spades,
-# * `TH` - ten of hearts,
-# * `3C` - three of clubs.
-
-# ### Best hand
-# `best_hand` function returns best hands for hands with rankable cards.
-
-# ### Best wild hand
-# `best_wild_hand` function returns best hands for hands with all cards, including jokers.
-# Jokers can be either red `R` (goes for hearts and diamonds) or black `B` (goes for spades and clubs) and may substitute any other card of its color.
-# Deck holds only 2 jokers.
-
-# ### Starting conditions
-# `hand_rank` was already written.
-# The following functions should have been written in order to proceed to `best_hand` and `best_wild_hand`:
-#
-# * `card_ranks` - should return a list of numerical equivalent of card ranks in a descending order;
-# * `flush` - should return True, if all cards share same suit;
-# * `straight` - should return True if sorted ranks make up an increasing sequence of 5 cards, where ranks differ by 1;
-# * `kind` - should return first encountered rank, for which there are n cards in a hand, or None, if no groups of a kind in a hand;
-# * `two_pair` - should return two ranks, which have 2 card each, if there are, else returns None.
 # -----------------
 
 from itertools import permutations
--- AdvancedBasicsPart_I/otus_hw1_poker/tests/test_poker.py
+++ AdvancedBasicsPart_I/otus_hw1_poker/tests/test_poker.py
@@ -75,7 +75,3 @@ class TestPoker(TestCase):
         self.assertEqual(sorted(best_hand("6C 7C 8C 9C TC 5C JS".split())), ['6C', '7C', '8C', '9C', 'TC'])
         self.assertEqual(sorted(best_hand("TD TC TH 7C 7D 8C 8S".split())), ['8C', '8S', 'TC', 'TD', 'TH'])
         self.assertEqual(sorted(best_hand("JD TC TH 7C 7D 7S 7H".split())), ['7C', '7D', '7H', '7S', 'JD'])
-
-
-
-
